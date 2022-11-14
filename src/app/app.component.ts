e6ede5df89f8fd8a144cd42466a04d02a093928c
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2022-11-14 09:26:56 +0100

    feat: first implement all quizzes (wip)
    They are hardcoded for now, will need to be refactored to use ngFor instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { quizzes } from './quizzes';
 
 @Component({
   selector: 'app-root',
--- src/app/quizz-card/quizz-card.component.ts
+++ src/app/quizz-card/quizz-card.component.ts
@@ -1,18 +1,41 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { quizzes } from '../quizzes';
 
 @Component({
   selector: 'app-quizz-card',
   templateUrl: './quizz-card.component.html',
   styleUrls: ['./quizz-card.component.scss']
 })
+
 export class QuizzCardComponent implements OnInit {
 
   constructor() { }
+
+  
   @Input() title: string = "";
   @Input() link: string = "";
   @Input() image: string = "";
-
+  
   ngOnInit(): void {
   }
+
+  quizzes = [
+    {
+      title: "Call center",
+      link: "home-office",
+      image: "./../../assets/images/call-center.svg"
+    },
+    {
+      title: "Take a break",
+      link: "home-office",
+      image: "./../../assets/images/take-a-brake.svg"
+    },
+    {
+      title: "Home office",
+      link: "home-office",
+      image: "./../../assets/images/home-office.svg"
+    }
+  ]
+
 
 }
