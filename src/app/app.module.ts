15178c1e981ccabd179548d209e5e9e164f49fcf
Myriam Mira <77079498+myracodes@users.noreply.github.com>
2022-11-17 10:29:05 +0100

    chore: move navbar to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { QuizzCardComponent } from './components/quizz-card/quizz-card.component';
 import { QuizzCategoriesComponent } from './components/quizz-categories/quizz-categories.component';
 import { QuestionCardComponent } from './components/question-card/question-card.component';
